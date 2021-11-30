bae29c2a4269974d3b39ed24b279b248d52651bb
akhmatsila <nik.demidoff24@gmail.com>
2021-11-30 12:59:32 +0300

    Fixed task added more tests

--- tasks/src/Task3.java
+++ tasks/src/Task3.java
@@ -1,13 +1,10 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Task3 {
-    /*
-    Determine whether a given non-blank string of parentheses is properly nested.
-    String consists only of the characters "(" and/or ")".
-    For example, string "(()(())())" is properly nested but string "())" isn't.
 
-    Try to estimate time-space complexitty.
-
-    Time complexity is O(n) , space complexity is O(1)
-     */
     public static boolean isProperParentheses(String str) {
         if (str.length() % 2 != 0) return false;
         int balance = 0;
@@ -17,5 +14,33 @@ public class Task3 {
             if (balance < 0) return false;
         }
         return balance == 0;
+    }
+
+    /*
+   Determine whether a given non-blank string of parentheses is properly nested.
+   String consists only of the characters "(" and/or ")".
+   For example, string "(()(())())" is properly nested but string "())" isn't.
+
+   Try to estimate time-space complexitty.
+
+   Time complexity is O(n) , space complexity is O(n)
+   Need to use Java Collections
+    */
+    public static boolean isProperNestedParentheses(String str) {
+        if (!isProperParentheses(str)) return false;
+        Deque<Character> leftHalf = new LinkedList<>();
+        Deque<Character> rightHalf = new LinkedList<>();
+        for (int i = 0 ; i < str.length() / 2 ; ++i) {
+            leftHalf.add(str.charAt(i));
+            rightHalf.add(str.charAt(str.length() - 1 - i));
+        }
+        Iterator<Character> leftHalfIter = leftHalf.iterator();
+        Iterator<Character> rightHalfIter = rightHalf.iterator();
+        while (leftHalfIter.hasNext()) {
+            char leftCurChar = leftHalfIter.next();
+            char rightCurChar = rightHalfIter.next();
+            if (leftCurChar == rightCurChar) return false;
+        }
+        return true;
     }
 }
--- tasks/test/Task3Test.java
+++ tasks/test/Task3Test.java
@@ -5,46 +5,61 @@ import static org.junit.Assert.*;
 public class Task3Test {
     @Test
     public void test1() {
-        assertTrue(Task3.isProperParentheses("()()()"));
+        assertTrue(Task3.isProperNestedParentheses("()()()"));
     }
 
     @Test
     public void test2() {
-        assertTrue(Task3.isProperParentheses("()(())"));
+        assertFalse(Task3.isProperNestedParentheses("()(())"));
     }
 
     @Test
     public void test3() {
-        assertTrue(Task3.isProperParentheses("(())()"));
+        assertFalse(Task3.isProperNestedParentheses("(())()"));
     }
 
     @Test
     public void test4() {
-        assertTrue(Task3.isProperParentheses("((()))"));
+        assertTrue(Task3.isProperNestedParentheses("((()))"));
     }
 
     @Test
     public void test5() {
-        assertFalse(Task3.isProperParentheses(")()()"));
+        assertFalse(Task3.isProperNestedParentheses(")()()"));
     }
 
     @Test
     public void test6() {
-        assertFalse(Task3.isProperParentheses("((())))"));
+        assertFalse(Task3.isProperNestedParentheses("((())))"));
     }
 
     @Test
     public void test7() {
-        assertFalse(Task3.isProperParentheses("((()))()("));
+        assertFalse(Task3.isProperNestedParentheses("((()))()("));
     }
 
     @Test
     public void test8() {
-        assertFalse(Task3.isProperParentheses(")()("));
+        assertFalse(Task3.isProperNestedParentheses(")()("));
     }
 
     @Test
     public void test9() {
-        assertFalse(Task3.isProperParentheses(")((()))("));
+        assertFalse(Task3.isProperNestedParentheses(")((()))("));
+    }
+
+    @Test
+    public void test10() {
+        assertTrue(Task3.isProperNestedParentheses("(((()()()))((()()()))((()()())))"));
+    }
+
+    @Test
+    public void test11(){
+        assertTrue(Task3.isProperNestedParentheses("()()()()()()()()()()()()"));
+    }
+
+    @Test
+    public void test12() {
+        assertTrue(Task3.isProperNestedParentheses("(())(((()()())))(())"));
     }
 }
